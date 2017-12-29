92b14d846946919e1e07c51f73f210bedeadb5d3
FrankWork <lzh00776@163.com>
2017-12-29 21:47:45 +0800

    update

--- src2/adv_semeval.py
+++ src2/adv_semeval.py
@@ -11,10 +11,11 @@ from tensor2tensor.data_generators import generator_utils
 from tensor2tensor.utils import registry
 
 # datasets
-LOCATION_OF_DATA = "data/SemEval/"
+LOCATION_OF_DATA = os.path.join(os.environ['ROOT_DIR'], "data/SemEval/")
 
 TRAIN_DATASETS = os.path.join(LOCATION_OF_DATA, 'train.txt')
 TEST_DATASETS =  os.path.join(LOCATION_OF_DATA, 'test.txt')
+LABEL_FILE = os.path.join(LOCATION_OF_DATA, 'relations.txt')
 
 class RawDataGenerator(object):
   '''Load text from file'''
@@ -22,18 +23,65 @@ class RawDataGenerator(object):
     self.entity_finder = re.compile(r"<e[12]>(.*?)</e[12]>")
     self.entity_tag_mask = re.compile(r"</?e[12]>")
 
-  def find_start_position(self, entity, sentence):
+  def find_start_position(self, entities, sentence):
     ''' find start position of the entity in sentence
     Args:
-      entity: a list of tokens
+      entities: a list of 2 entities, each entity is a list of tokens
       sentence: a list of tokens
     '''
-    n = len(entity)
-    for i in range(len(sentence)):
-      if sentence[i:i+n]==entity:
-        # first, last = i, i+n-1
-        return i
-    return -1
+    pos = []
+    for entity in entities:
+      n = len(entity)
+      for i in range(len(sentence)):
+        if sentence[i:i+n]==entity:
+          # first, last = i, i+n-1
+          pos.append(i)
+    return pos
+
+  def relative_distance(self, n):
+    '''convert relative distance to positive number
+    -60), [-60, 60], (60
+    '''
+    if n < -60:
+      return 0
+    elif n >= -60 and n <= 60:
+      return n + 61
+    
+    return 122
+
+  def position_feature(self, ent_pos, sentence):
+    '''
+    Args:
+      ent_pos: int, start position of the entity
+      sentence: a list of tokens
+    '''
+    length = len(sentence)
+    return [self.relative_distance(i-ent_pos) for i in range(length)]
+  
+  def entity_context(self, ent_pos, sentence):
+    ''' return [w(e-1), w(e), w(e+1)]
+    '''
+    context = []
+    context.append(sentence[ent_pos])
+
+    if ent_pos >= 1:
+      context.append(sentence[ent_pos-1])
+    else:
+      context.append(sentence[ent_pos])
+    
+    if ent_pos < len(sentence)-1:
+      context.append(sentence[ent_pos+1])
+    else:
+      context.append(sentence[ent_pos])
+    
+    return context 
+  
+  def lexical_feature(self, entities_pos, sentence):
+    context1 = self.entity_context(entities_pos[0], sentence)
+    context2 = self.entity_context(entities_pos[1], sentence)
+
+    # ignore WordNet hypernyms in paper
+    return context1 + context2
 
   def generator(self, data_file, for_vocab=False):
     '''load raw data from text file, 
@@ -43,6 +91,10 @@ class RawDataGenerator(object):
       Comment: Not a collection: there is structure here, organisation.
 
       EOF
+    Returns:
+      label: string
+      entities: list of string, len == 2
+      sentence: string
     '''
     lines = open(data_file).readlines()
     n = len(lines)
@@ -83,18 +135,24 @@ class AdvSemEval(problem.Problem):
     # Generate vocab
     raw_gen = RawDataGenerator()
     
-    text_encoder = generator_utils.get_or_generate_vocab_inner(
+    vocab_encoder = generator_utils.get_or_generate_vocab_inner(
         data_dir, self.vocab_file, self.targeted_vocab_size,
         raw_gen.generator(data_file, for_vocab=True))
+    label_encoder = text_encoder.ClassLabelEncoder(class_labels_fname=LABEL_FILE)
 
     # Generate examples
     for label, entities, sentence in raw_gen.generator(data_file):
-      entities = [text_encoder.encode(e) for e in entities]
-      sentence = text_encoder.encode(sentence)
-      raw_gen.find_start_position()
+      entities = [vocab_encoder.encode(e) for e in entities]
+      sentence = vocab_encoder.encode(sentence)
+
+      entities_pos = raw_gen.find_start_position(entities, sentence)
+      
       yield {
-          "inputs": text_encoder.encode(doc) + [EOS],
-          "targets": [int(label)],
+          "inputs": sentence,
+          "targets": [label_encoder.encode(label)],
+          'lexicail': raw_gen.lexical_feature(entities_pos, sentence),
+          'position1': raw_gen.position_feature(entities_pos[0], sentence),
+          'position2': raw_gen.position_feature(entities_pos[1], sentence),
       }
 
   def generate_data(self, data_dir, tmp_dir, task_id=-1):
--- src2/test_tokenizer.py
+++ src2/test_tokenizer.py
@@ -6,19 +6,20 @@ doc = ["I've got it! Don't you see? :)",
 "188BET is an online sportsbook provider. 188BET is owned by Cube Limited",
 "Avista Utilities is a U.S. energy company.",
 "it has 18ml.",
-"''i've got it!!!"]
+"''i've got it!!!",
+"小燕子穿花衣，年年春天来这里。"]
 
-# for text in doc:
-#   tokens = tokenizer.encode(text)
-#   print(tokens)
+for text in doc:
+  tokens = tokenizer.encode(text)
+  print(tokens)
 
-import sys
-import six
-import unicodedata
+# import sys
+# import six
+# import unicodedata
 
-_ALPHANUMERIC_CHAR_SET = set(
-    six.unichr(i) for i in range(sys.maxunicode)
-    if (unicodedata.category(six.unichr(i)).startswith("L") or
-        unicodedata.category(six.unichr(i)).startswith("N")))
-n = len(_ALPHANUMERIC_CHAR_SET)
-print(n)
+# _ALPHANUMERIC_CHAR_SET = set(
+#     six.unichr(i) for i in range(sys.maxunicode)
+#     if (unicodedata.category(six.unichr(i)).startswith("L") or
+#         unicodedata.category(six.unichr(i)).startswith("N")))
+# n = len(_ALPHANUMERIC_CHAR_SET)
+# print(n)
