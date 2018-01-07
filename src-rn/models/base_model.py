e73bcf1848211e3f947be3e0e6fc9a80739f9fc8
FrankWork <lzh00776@163.com>
2018-01-07 22:14:23 +0800

    att + conv

--- src-rn/models/base_model.py
+++ src-rn/models/base_model.py
@@ -1,10 +1,11 @@
 import os
 import tensorflow as tf
+from tensorflow.python.framework import ops
 
 flags = tf.app.flags
 flags.DEFINE_string("logdir", "saved_models/", "where to save the model")
-
 FLAGS = tf.app.flags.FLAGS
+
 
 class BaseModel(object):
 
@@ -26,53 +27,6 @@ class BaseModel(object):
     self.saver.save(session, self.save_path, global_step)
 
 
-def conv_block(inputs, input_dim, kernels_size, num_filters, name, training, 
-               batch_norm=False, initializer=None, shortcut=None, reuse=None):
-  inputs = tf.expand_dims(inputs, axis=-1)
-
-  with tf.variable_scope(name, reuse=reuse):
-    conv_outs = []
-    for kernel_size in kernels_size:
-      conv_weight = tf.get_variable('W-%d'%kernel_size, 
-                              [kernel_size, input_dim, 1, num_filters],
-                              initializer=initializer)
-      conv_bias = tf.get_variable('b-%d'%kernel_size, [num_filters], 
-                              initializer=tf.constant_initializer(0.1))
-      conv_out = tf.nn.conv2d(inputs, conv_weight,
-                          strides=[1, 1, input_dim, 1], padding='SAME')
-
-      # conv_out = tf.layers.conv1d(inputs, num_filters, kernel_size,
-      #               strides=input_dim, padding='same',
-      #               kernel_initializer=initializer,
-      #               name='conv-%d' % kernel_size,
-      #               reuse=reuse)
-      if batch_norm:
-        conv_out = tf.layers.batch_normalization(conv_out, training=training)
-      conv_out = tf.nn.relu(conv_out + conv_bias)
-      if shortcut is not None:
-        conv_out = conv_out + shortcut
-      conv_outs.append(conv_out)
-    return conv_outs
-
-
-def max_pool(conv_outs, max_len, num_filters_in_conv, flat=True):
-  pool_outs = []
-
-  for conv in conv_outs:
-    pool = tf.nn.max_pool(conv, ksize= [1, max_len, 1, 1], 
-                              strides=[1, max_len, 1, 1], padding='SAME')
-    # pool = tf.layers.max_pooling1d(conv, max_len, max_len, padding='same')
-    pool_outs.append(pool)
-    
-  n = len(conv_outs)
-  pools = tf.concat(pool_outs, 3)
-  if flat:
-    pools = tf.reshape(pools, [-1, n*num_filters_in_conv])
-  else:
-    pools = tf.squeeze(pools)
-
-  return pools
-
 def conv_block_v2(inputs, kernel_size, num_filters, name, training, 
                batch_norm=False, initializer=None, shortcut=None, reuse=None):
   with tf.variable_scope(name, reuse=reuse):
@@ -87,15 +41,6 @@ def conv_block_v2(inputs, kernel_size, num_filters, name, training,
     if shortcut is not None:
       conv_out = conv_out + shortcut
     return conv_out
-
-def pad(tensor, in_filter, out_filter):
-  if out_filter != in_filter:
-    n = out_filter-in_filter
-    n1 = n//2
-    n2 = n - n1
-    return tf.pad(tensor, [[0,0], [0,0], [n1, n2]])
-  return tensor
-  
 
 def optimize(loss, lrn_rate, max_norm=None, decay_steps=None):
   update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # for batch_norm
@@ -114,5 +59,3 @@ def optimize(loss, lrn_rate, max_norm=None, decay_steps=None):
     train_op = optimizer.apply_gradients(zip(gradients, variables), global_step=global_step)
     return train_op
 
-  
-  
--- src-rn/models/cnn_model.py
+++ src-rn/models/cnn_model.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 from models.base_model import * 
+from models.attention import *
 from models.residual import residual_net
 
 flags = tf.app.flags
 
 flags.DEFINE_integer("pos_num", 123, "number of position feature")
 flags.DEFINE_integer("pos_dim", 5, "position embedding size")
+flags.DEFINE_integer("num_hops", 1, "hop numbers of entity attention")
 flags.DEFINE_float("l2_coef", 0.01, "l2 loss coefficient")
-flags.DEFINE_float("keep_prob", 0.5, "dropout keep probability")
-flags.DEFINE_integer("num_filters", 100, "cnn number of output unit")
+flags.DEFINE_float("dropout_rate", 0.5, "dropout probability")
 flags.DEFINE_float("lrn_rate", 0.001, "learning rate")
 
 FLAGS = flags.FLAGS
@@ -44,96 +45,113 @@ class CNNModel(BaseModel):
                                       dtype=tf.float32,
                                       trainable=w_trainable)
     # self.word_embed = self.normalize_embed(self.word_embed, vocab_freq)
-    pos_shape = [FLAGS.pos_num, FLAGS.pos_dim]  
+    pos_shape = [FLAGS.pos_num, FLAGS.pos_dim]
     self.pos1_embed = tf.get_variable('pos1_embed', shape=pos_shape)
     self.pos2_embed = tf.get_variable('pos2_embed', shape=pos_shape)
 
-    # self.embed_dim = self.word_dim + 2*FLAGS.pos_dim
+    self.embed_dim = self.word_dim + 2* FLAGS.pos_dim
+
     self.tensors = dict()
 
     with tf.variable_scope('semeval_graph'):
       self.build_semeval_graph(semeval_data)
 
   def bottom(self, data):
-    lexical, labels, length, sentence, pos1, pos2 = data
+    (label, length, 
+      sentence, position1, position2, 
+      ent1_toks, ent1_pos1, ent1_pos2,
+      ent2_toks, ent2_pos1, ent2_pos2,
+      context, cont_pos1, cont_pos2) = data
 
-    # embedding lookup
-    # weight = self.word_dim**0.5
-    lexical = tf.nn.embedding_lookup(self.word_embed, lexical)
-    # lexical *= weight
-    # lexical = scale_l2(lexical)
-
+    # sentence and pos from embedding
     sentence = tf.nn.embedding_lookup(self.word_embed, sentence)
-    # sentence *= weight
-    # sentence = scale_l2(sentence)
 
-    pos1 = tf.nn.embedding_lookup(self.pos1_embed, pos1)
-    pos2 = tf.nn.embedding_lookup(self.pos2_embed, pos2)
+    pos1 = tf.nn.embedding_lookup(self.pos1_embed, position1)
+    pos2 = tf.nn.embedding_lookup(self.pos2_embed, position2)
 
-    lexical = tf.reshape(lexical, [-1, 6*self.word_dim])
+    # entities, context and pos from memory
+    ent1_toks = tf.nn.embedding_lookup(self.word_embed, ent1_toks)
+    ent2_toks = tf.nn.embedding_lookup(self.word_embed, ent2_toks)
+    context = tf.nn.embedding_lookup(self.word_embed, context)
 
-    return lexical, labels, length, sentence, pos1, pos2
+    ent1_pos1 = tf.nn.embedding_lookup(self.pos1_embed, ent1_pos1)
+    ent1_pos2 = tf.nn.embedding_lookup(self.pos2_embed, ent1_pos2)
+    ent2_pos1 = tf.nn.embedding_lookup(self.pos1_embed, ent2_pos1)
+    ent2_pos2 = tf.nn.embedding_lookup(self.pos2_embed, ent2_pos2)
+    cont_pos1 = tf.nn.embedding_lookup(self.pos1_embed, cont_pos1)
+    cont_pos2 = tf.nn.embedding_lookup(self.pos2_embed, cont_pos2)
 
-  def body_shallow(self, lexical, sentence, pos1, pos2):
-    # num_filters = [60, 125, 300, 60]
-    num_filters = [350, 400, 450, 500]
-    self.layers = []
+    # concat pos embedding to entities and context
+    context = tf.concat([context, cont_pos1, cont_pos2], axis=2)
+    ent1 = tf.concat([ent1_toks, ent1_pos1, ent1_pos2], axis=2)
+    ent2 = tf.concat([ent2_toks, ent2_pos1, ent2_pos2], axis=2)
 
-    sentence = tf.layers.dropout(sentence, 1-FLAGS.keep_prob, training=self.is_train)
-    sent_pos = tf.concat([sentence, pos1, pos2], axis=2)
-    self.layers.append(sent_pos)
+    return label, sentence, pos1, pos2, context, ent1, ent2
 
-    # conv 1
-    conv_input = self.layers[-1]
-    conv_out = conv_block_v2(conv_input, KERNEL_SIZE, num_filters[0],
+  def conv_shallow(self, inputs):
+    conv_out = conv_block_v2(inputs, KERNEL_SIZE, NUM_FILTERS,
                             'conv_block1',training=self.is_train, 
                           initializer=self.he_normal, batch_norm=False)
 
     pool_out = tf.layers.max_pooling1d(conv_out, MAX_LEN, MAX_LEN, padding='same')
     pool_out = tf.squeeze(pool_out, axis=1)
-    self.layers.append(pool_out)
+    return pool_out
 
-  
-    body_out = tf.concat([lexical, self.layers[-1]], axis=1)
-    return body_out
-    # return None
-  
-  def body(self, lexical, sentence, pos1, pos2):
-    sentence = tf.layers.dropout(sentence, 1-FLAGS.keep_prob, training=self.is_train)
+  def conv_deep(self, inputs):
+    return residual_net(inputs, MAX_LEN, NUM_FILTERS, self.is_train)
+
+  def body(self, sentence, pos1, pos2, context, ent1, ent2):
+    lexical = self.entity_attention(context, ent1, ent2, FLAGS.num_hops)
+
+    sentence = tf.layers.dropout(sentence, FLAGS.dropout_rate, training=self.is_train)
     sent_pos = tf.concat([sentence, pos1, pos2], axis=2)
+    conv_out = self.conv_shallow(sent_pos)
 
-    res_out = residual_net(sent_pos, MAX_LEN, NUM_FILTERS, self.is_train, NUM_CLASSES)
-    body_out = tf.concat([lexical, res_out], axis=1)
-    # # 1 conv
-    # conv_input = sent_pos
-    # conv_out = conv_block_v2(conv_input, KERNEL_SIZE, NUM_FILTERS,
-    #                         'conv_block_final', training=self.is_train,
-    #                         initializer=self.he_normal, batch_norm=False)
-    # pool_out = tf.layers.max_pooling1d(conv_out, MAX_LEN, MAX_LEN, padding='same')
-    # body_out = tf.squeeze(pool_out, axis=1)
-
-
-    body_out = tf.layers.dropout(body_out, 1-FLAGS.keep_prob, training=self.is_train)
+    body_out = tf.concat([lexical, conv_out], axis=1)
+    
+    body_out = tf.layers.dropout(body_out, FLAGS.dropout_rate, training=self.is_train)
     return body_out
-    # return None
+
+  def entity_attention(self, context, ent1, ent2, num_hops):
+    cont1 = context
+    cont2 = context
+    for i in range(num_hops):
+      cont1 = multihead_attention(cont1, ent1, num_heads=10, 
+                                  dropout_rate=FLAGS.dropout_rate,
+                                  is_training=self.is_train, scope='att1%d'%i)
+      cont2 = multihead_attention(cont2, ent2, num_heads=10, 
+                                  dropout_rate=FLAGS.dropout_rate,
+                                  is_training=self.is_train, scope='att2%d'%i)
+      # cont1 = feedforward(cont1, num_units=[620, 310], scope='ffd1%d'%i)
+      # cont2 = feedforward(cont2, num_units=[620, 310], scope='ffd2%d'%i)
+      ent1 = cont1
+      ent2 = cont2
+
+    ent1 = tf.reduce_max(ent1, axis=1) # (batch, embed)
+    ent2 = tf.reduce_max(ent2, axis=1)
+    entities = tf.concat([ent1, ent2], axis=-1)
+    # entities = tf.squeeze(entities, axis=1)
+    
+    return entities
 
   def top(self, body_out, labels):
     logits = tf.layers.dense(body_out, NUM_CLASSES, kernel_regularizer=self.regularizer)
 
     # Calculate Mean cross-entropy loss
     with tf.name_scope("loss"):
+      one_hot = tf.one_hot(labels, NUM_CLASSES)
+      # one_hot = label_smoothing(one_hot)
       cross_entropy = tf.losses.softmax_cross_entropy(logits=logits, 
-                           onehot_labels=tf.one_hot(labels, NUM_CLASSES))
+                           onehot_labels=one_hot)
 
       regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
       loss = cross_entropy + sum(regularization_losses)
-      # loss = cross_entropy #+ FLAGS.l2_coef * tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
 
     return logits, loss
 
   def build_semeval_graph(self, data):
-    lexical, labels, length, sentence, pos1, pos2 = self.bottom(data)
-    body_out = self.body(lexical, sentence, pos1, pos2)
+    labels, sentence, pos1, pos2, context, ent1, ent2 = self.bottom(data)
+    body_out = self.body(sentence, pos1, pos2, context, ent1, ent2)
     logits, loss = self.top(body_out, labels)
 
     # Accuracy
